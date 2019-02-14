cb5d79389d98aa198e32663f1c211e0514f3ec8b
Aubrey Te Kanawa <aubreytekanawa@gmail.com>
2019-02-15 12:21:45 +1300

    grant and aubrey conquering the cart.. with Francines help

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux'
 import isLoading from './isLoading'
 import giftee from './giftee'
 import users from './users'
+import gifts from './gifts'
 
 // import otherReducer from './other-reducer'
 
 export default combineReducers({
   isLoading,
   giftee,
-  users
+  users,
+  gifts
 })
