fe3f755e1c5a65e711781ffccf9d8e01e1b9c89b
ngapeita <ngapeita@.com>
2019-02-15 09:45:05 +1300

    added a little more seed data

--- seeds/gifts.js
+++ seeds/gifts.js
@@ -4,12 +4,12 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('gifts').insert([
-        {id: 1, user_id: 1, item: 'rocket launcher', url: 'https://www.alexbrands.com/product/outdoor-sports/blasteroid-rocket-with-launcher/', purchased: false},
-        {id: 2, user_id: 3, item: 'tank', url: 'https://www.ebay.com/itm/WW2-Military-Tank-Collectible-1950-s-Vintage-Army-/392234154329', purchased: false},
+        {id: 1, user_id: 1, item: 'rocket launcher', url: 'https://www.gunbroker.com/item/798196273', purchased: false},
+        {id: 2, user_id: 5, item: 'tank', url: 'https://www.ebay.com/itm/WW2-Military-Tank-Collectible-1950-s-Vintage-Army-/392234154329', purchased: false},
         {id: 3, user_id: 3, item: 'laserbeam', url: 'https://www.ebay.com/itm/10000m-532nm-301-Green-Laser-Pointer-Lazer-Pen-Visible-Beam-Light-High-Power/401443320860?hash=item5d77e2f41c:g:y9MAAOSwWqpavZPv:rk:8:pf:0', purchased: false},
         {id: 4, user_id: 2, item: 'nightvision', url: 'https://www.ebay.com/itm/Nightfox-100V-Night-Vision-Monocular-Binoculars-Digital-Infrared-IR-3x20/232369188200?epid=515475158&hash=item361a486d68:g:BCAAAOSwhH1cPyHp:rk:1:pf:0', purchased: false},
-        {id: 5, user_id: 1, item: 'robot', url: 'https://www.ebay.com/itm/ROT2U-6DOF-Aluminium-Robot-Arm-Clamp-Claw-Mount-Kit-w-Servos-for-Arduino-Silver/371815607274?epid=2109003104&hash=item5691efebea:g:SwkAAOSwtItbaqni:rk:7:pf:0', purchased: false},
-        {id: 6, user_id: 2, item: 'robot', url: 'https://www.ebay.com/itm/ROT2U-6DOF-Aluminium-Robot-Arm-Clamp-Claw-Mount-Kit-w-Servos-for-Arduino-Silver/371815607274?epid=2109003104&hash=item5691efebea:g:SwkAAOSwtItbaqni:rk:7:pf:0', purchased: false}
+        {id: 5, user_id: 4, item: 'robot', url: 'https://www.ebay.com/itm/ROT2U-6DOF-Aluminium-Robot-Arm-Clamp-Claw-Mount-Kit-w-Servos-for-Arduino-Silver/371815607274?epid=2109003104&hash=item5691efebea:g:SwkAAOSwtItbaqni:rk:7:pf:0', purchased: false},
+        {id: 6, user_id: 2, item: 'drone', url: 'https://myfirstdrone.com/drones-for-sale', purchased: false}
 
       ])
     })
--- seeds/users.js
+++ seeds/users.js
@@ -6,7 +6,10 @@ exports.seed = function (knex, Promise) {
       return knex('users').insert([
         {id: 1, name: 'Blossum', email: 'blossum@powerpuff.com'},
         {id: 2, name: 'Bubbles', email: 'bubbles@powerpuff.com'},
-        {id: 3, name: 'Buttercup', email: 'buttercup@powerpuff.com'}
+        {id: 3, name: 'Buttercup', email: 'buttercup@powerpuff.com'},
+        {id: 4, name: 'Mojo Jojo', email: 'mojo@badguys.com'},
+        {id: 5, name: 'Professor Utonium', email: 'professor@smartguys.com'},
+        {id: 6, name: 'Sara Bellum', email: 'sara@powerpuff.com'}
       ])
     })
 };
