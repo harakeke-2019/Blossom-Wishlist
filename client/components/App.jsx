d7ac1d02c5d93f3a844ed07cc706313501c42eae
Aubrey Te Kanawa <aubreytekanawa@gmail.com>
2019-02-15 15:47:26 +1300

    finished sort of

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,17 +3,26 @@ import Users from './Users'
 import Cart from './Cart'
 import WishList from './WishList'
 import AddGift from './AddGift'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 
 const App = (props) => {
   return (
-    <div>
-      <Users />
-      <Cart />
-      {props.giftee !== 0 && <WishList />}
-      <AddGift />
+    <body className= "background">
+      <header className= "navbar">BLOSSOMS AWESOME CHRISTMAS WISHLIST APP</header>
+      <div className="row">
+        <div className="column left" ><h2>USERS</h2>
+          <p><Users /></p>
+        </div>
+        <div className="column right"><h2>WISH LIST </h2>
+          <p>{props.giftee !== 0 && <WishList />}</p>
+          <p><AddGift /></p>
+        </div>
+        <div className="column middle"><h2>CART</h2>
+          <p><Cart /></p>
+        </div>
+      </div>
+    </body>
 
-    </div>
   )
 }
 
